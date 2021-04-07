2d2d0792d66f0677711eba0c820726d4e62909a4
StarFoxy <ctc5000@yandex.ru>
2021-04-08 01:03:43 +0400

    change server

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get("/", (req, res) => {
 
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8001;
 require("./app/routes/routesTable.routes")(app);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
